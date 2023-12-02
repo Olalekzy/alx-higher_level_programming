fb4c6711f576b180a0a11eb74f37359b98da6e7b
Olalekzy <olalekzy98@gmail.com>
2023-12-02 06:29:37 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,51 +3,48 @@
 #include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
+ * is_palindrome - Checks if a linked list is a palindrome
+ * @head: Pointer to the head of the list
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
-
-	if (*head == NULL || head2->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (head2 != NULL)
+
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev_slow = *head;
+	listint_t *second_half = NULL;
+	listint_t *mid_node = NULL;
+	int is_palindrome = 1;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
+		fast = fast->next->next;
+		prev_slow = slow;
+		slow = slow->next;
 	}
-	aux2 = aux;
-	while (*head != NULL)
+
+	if (fast != NULL) // Odd number of elements, move to the next node
 	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
+		mid_node = slow;
+		slow = slow->next;
 	}
-	free_listint(aux);
-	return (1);
+
+	second_half = slow;
+	prev_slow->next = NULL;
+	reverse_list(&second_half); // Implement reverse_list() to reverse the second half of the list
+
+	is_palindrome = compare_lists(*head, second_half);
+
+	if (mid_node != NULL) // Reconstructing the original list
+	{
+		prev_slow->next = mid_node;
+		mid_node->next = second_half;
+	}
+	else
+		prev_slow->next = second_half;
+
+	return is_palindrome;
 }
