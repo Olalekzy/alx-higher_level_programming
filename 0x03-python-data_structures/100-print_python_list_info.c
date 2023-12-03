ca387d17d6208c28351c7b00fae55bc516b1777d
Olalekzy <olalekzy98@gmail.com>
2023-12-03 14:00:43 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,34 @@
-void print_python_list_info(PyObject *p) 
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - function that prints basic info about Python lists
+ * @p: pointer to a PyObject representing a Python list
+ */
+void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size, i;
-    PyObject *item;
+    Py_ssize_t size, i;  // Use Py_ssize_t for Python's size type
 
-    if (!PyList_Check(p)) 
-    {
-        fprintf(stderr, "Invalid Python List!\n");
-        return;
-    }
+    // Print the size of the list
+    printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
 
-    size = PyList_Size(p);
-    printf("[*] Size of the Python List = %ld\n", size);
+    // Print the memory allocated for the list
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < size; i++) 
+    // Print info about each element in the list
+    size = Py_SIZE(p);
+    for (i = 0; i < size; i++)
     {
-        item = PyList_GetItem(p, i);
-        if (item != NULL) 
-	{
+        PyObject *item = PyList_GetItem(p, i);
+        if (item != NULL)
+        {
+            // Print the type name of each element
             printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-        } else 
-	{
+        }
+        else
+        {
+            // Handle the case where getting the item fails
             fprintf(stderr, "Failed to retrieve element %ld\n", i);
         }
     }
