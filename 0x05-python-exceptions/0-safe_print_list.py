c7ae09cd268ea64fdbea81f02ecf685fb9414c67
Olalekzy <olalekzy98@gmail.com>
2023-12-18 13:19:42 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,16 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     sum_total = 0
-    index = 0
-
-    while index < x:
+    for i in range(x):
         try:
-            print(my_list[index], end="")
+            print(f"{my_list[i]}", end="")
             sum_total += 1
         except IndexError:
             break
-        index += 1
-
     print()
-    return sum_total
-
+    return(sum_total)
